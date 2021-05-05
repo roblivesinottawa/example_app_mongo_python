79609918da0645753726a632274b563b076c7154
Robson Ferreira <tech.rob@icloud.com>
2021-05-05 15:32:14 -0400

    added new routes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 app.config['MONGO_DB'] = 'mongopydb'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/mongopydb'
+app.config['MONGO_URI'] = '*********************'
 
 mongo = PyMongo(app)
 
@@ -18,3 +18,29 @@ def get_all_comics():
     for com in comic.find():
         output.append({'name': com['name'], 'publisher': com['publisher']})
     return jsonify({'result': output})
+
+
+@app.route('/comic', methods=['GET'])
+def get_one_comic(name):
+    comic = mongo.db.comics
+    comic_ = comic.find_one({'name': name})
+    if comic_:
+        output = ({'name': comic_['name'], 'publisher': comic_['publisher']})
+    else:
+        output = "No such name"
+    return jsonify({'result': output})
+
+
+@app.route('/comic', methods=['POST'])
+def add_comic():
+    comic = mongo.db.comics
+    name = request.json['name']
+    publisher = request.json['publisher']
+    comic_id = comic.insert({'name': name, 'publisher': publisher})
+    new_comic = comic.find_one({'_id': comic_id})
+    output = ({'name': new_comic['name'], 'publisher': new_comic['publisher']})
+    return jsonify({'result': output})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
